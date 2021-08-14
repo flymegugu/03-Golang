7a6bfd07889ea9c628580d5ec89fb10682471b25
MaxGu <MaxGu>
2021-08-14 16:49:34 +0800

    add

---
+++ 1.go
@@ -0,0 +1,14 @@
+/*
+ * @Author: [MaxGu]
+ * @Date: 2021-07-29 11:39:31
+ * @LastEditors: [MaxGu]
+ * @LastEditTime: 2021-07-29 11:40:54
+ * @Description:
+ */
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
